0fe36a143d6dcaa2bd7a7ec9e4dfb9493375b80a
unknown <m.alperenince00@gmail.com>
2023-01-07 01:47:37 +0300

    New AnchorConstraint added

---
+++ Runtime/Constraints/VerletAnchorConstraint.cs
@@ -0,0 +1,39 @@
+﻿using RopeSystem.Runtime.Particles;
+using UnityEngine;
+
+namespace RopeSystem.Runtime.Constraints
+{
+    public class VerletAnchorConstraint : IVerletParticleConstraint
+    {
+        public IVerletParticle AnchoredParticle
+        {
+            get => _anchoredParticle;
+            set
+            {
+                _anchoredParticle = value;
+                _anchoredParticle.IsPinned = true;
+                _anchoredParticle.Position = AnchorTransform.position;
+            }
+        }
+
+        public Transform AnchorTransform
+        {
+            get => _anchorTransform;
+            set => _anchorTransform = value;
+        }
+
+        private Transform _anchorTransform;
+        private IVerletParticle _anchoredParticle;
+
+        public VerletAnchorConstraint(IVerletParticle anchoredParticle, Transform anchorTransform)
+        {
+            AnchorTransform = anchorTransform;
+            AnchoredParticle = anchoredParticle;
+        }
+        
+        public void ResolveConstraint()
+        {
+            AnchoredParticle.Position = AnchorTransform.position;
+        }
+    }
+}
